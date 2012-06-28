67f01d07482670b3ff88a121351872de8453a654
Yamagishi Kazutoshi <ykzts@desire.sh>
2012-06-28 13:15:17 +0000

    first commit

---
+++ tateya.or.jp.lua
@@ -0,0 +1,19 @@
+
+a(_a, "207.97.227.245")
+
+txt(_a, "google-site-verification=werBkffZVWFVv8HAtdnZ8uH0_XOhGGvuG5nEORTllnA")
+txt(_a, "google-site-verification=92qBPnbvI1RJ03_944hwC3F3P4-0BfeI_fPvt1WaPyM")
+
+mx(_a, "ASPMX.L.GOOGLE.COM", 1)
+mx(_a, "ALT1.ASPMX.L.GOOGLE.COM", 5)
+mx(_a, "ALT2.ASPMX.L.GOOGLE.COM", 5)
+mx(_a, "ASPMX2.GOOGLEMAIL.COM", 10)
+mx(_a, "ASPMX3.GOOGLEMAIL.COM", 10)
+
+cname(concat("www", _a), _a)
+
+cname(concat("kai", _a), _a)
+
+cname(concat("mail", _a), "ghs.google.com")
+
+cname(concat("news", _a), "domains.tumblr.com")
